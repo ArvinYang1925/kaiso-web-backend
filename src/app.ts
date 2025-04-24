c58a944654c33d754ab66fb0139da156577851dd
ArvinYang1925 <arvinyang1925@gmail.com>
2025-04-24 15:33:48 +0800

    feat: 🎸 add 404 not found middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import express from "express";
+import express, { Request, Response } from "express";
 import { AppDataSource } from "./config/db";
 import todoRoutes from "./routes/todoRoutes";
 import userRoutes from "./routes/userRoutes";
@@ -18,6 +18,23 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
+// 設定 404 處理中間件
+app.use((_req: Request, res: Response) => {
+  res.status(404).json({
+    status: "failed",
+    message: "無此路由",
+  });
+});
+
+// 增加錯誤處理中間件
+// app.use((err: Error, _req: Request, res: Response) => {
+//   console.error(err.stack);
+//   res.status(500).json({
+//     status: "error",
+//     message: "伺服器發生錯誤",
+//   });
+// });
 
 AppDataSource.initialize()
   .then(() => {
